3ae2c8252fb92ab558fc51d8f201e9e811e112a6
freakboy791 <45658876+freakboy791@users.noreply.github.com>
2025-08-15 20:52:37 +0000

    update emails

--- utils/email.ts
+++ utils/email.ts
@@ -20,7 +20,7 @@ export async function sendEmail(to: string, subject: string, html: string) {
   if (!from) throw new Error("FROM_EMAIL not configured");
 
   const res = await resend.emails.send({
-    from: `Asset Manager <${from}>`,
+    from: `assetTRAC Admin <${from}>`,
     to,
     subject,
     html,
